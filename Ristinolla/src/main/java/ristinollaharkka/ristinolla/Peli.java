18d6c907186b86b12d2153fb0c74ac3db04e0e51
wametsol <waltteri.metsola@gmail.com>
2014-02-05 16:29:34 +0200

    5.2.14 sääntöjä + käyttistä

--- Ristinolla/src/main/java/ristinollaharkka/ristinolla/Peli.java
+++ Ristinolla/src/main/java/ristinollaharkka/ristinolla/Peli.java
@@ -86,7 +86,7 @@ public class Peli extends JFrame {
                 tarkistaNormaali();
             }
             if(vaikeus == 5){
-                
+                tarkistaVaikea();
             }
             
         }
@@ -176,9 +176,13 @@ public class Peli extends JFrame {
             }
             
             if (voittaja.equals("X") && i == 0){
+                frame.setVisible(false);
+                voittoIkkuna(this.yksi);
                 System.out.println("Onneksi olkoon " + this.yksi.pelaajanNimi() + "! VOITIT PELIN!");
             }
             if (voittaja.equals("O") && i == 1){
+                frame.setVisible(false);
+                voittoIkkuna(this.kaksi);
                 System.out.println("Onneksi olkoon " + this.kaksi.pelaajanNimi() + "! VOITIT PELIN!");
             }
             i++;
@@ -197,6 +201,7 @@ public class Peli extends JFrame {
                     u = 0;
                 }
                 while(u<64){
+                    
                     if (u>4 && u<8){
                         
                     }
@@ -229,10 +234,55 @@ public class Peli extends JFrame {
                                 }
                             }
                         }
+                        if(nappeja[u+9].getText().equals(kirjain)){
+                                    if(nappeja[u+18].getText().equals(kirjain)){
+                                        if(nappeja[u+27].getText().equals(kirjain)){
+                                            voittaja = kirjain;
+                                        }
+                                    }
+                                }
+                        
+                    }
+//                    if (u<40){
+//                        if(nappeja[u].getText().equals(kirjain)){
+//                                if(nappeja[u+9].getText().equals(kirjain)){
+//                                    if(nappeja[u+18].getText().equals(kirjain)){
+//                                        if(nappeja[u+27].getText().equals(kirjain)){
+//                                            voittaja = kirjain;
+//                                        }
+//                                    }
+//                                }
+//                            }
+//                        }
+                    if ((u>2&&u<7)||(u>10&&u<15)||(u>18&&u<23)||(u>26&&u<31)||(u>32&&u<39)){
+                        if(nappeja[u].getText().equals(kirjain)){
+                                if(nappeja[u+7].getText().equals(kirjain)){
+                                    if(nappeja[u+14].getText().equals(kirjain)){
+                                        if(nappeja[u+21].getText().equals(kirjain)){
+                                            voittaja = kirjain;
+                                        }
+                                    }
+                                }
+                            }
+                        
+                        }
+                    if(u<40){
+                        if(nappeja[u].getText().equals(kirjain)){
+                            if(nappeja[u+8].getText().equals(kirjain)){
+                                if(nappeja[u+16].getText().equals(kirjain)){
+                                    if(nappeja[u+24].getText().equals(kirjain)){
+                                        voittaja = kirjain;
+                                    }
+                                }
+                            }
+                            
+                        }
                     }
                     u++;
                     }
                         if (voittaja.equals("X") && i == 0){
+                            frame.setVisible(false);
+                            voittoIkkuna(this.yksi);
                         System.out.println("Onneksi olkoon " + this.yksi.pelaajanNimi() + "! VOITIT PELIN!");
                     }
                     if (voittaja.equals("O") && i == 1){
@@ -241,7 +291,117 @@ public class Peli extends JFrame {
                 i++;
             }
         }
+        public void tarkistaVaikea(){
+            int i = 0;
+            int u = 0;
+            String voittaja = "";
+            String kirjain = "X";
+            while (i<2){
+                if(i==1){
+                    kirjain = "0";
+                    u = 0;
+                }
+                while(u<64){
+                if(u<96){
+                if(nappeja[u].getText().equals(kirjain)){
+                                if(nappeja[u+12].getText().equals(kirjain)){
+                                    if(nappeja[u+24].getText().equals(kirjain)){
+                                        if(nappeja[u+36].getText().equals(kirjain)){
+                                            if(nappeja[u+48].getText().equals(kirjain)){
+                                                voittaja = kirjain;
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                if ((u>3&&u<12)||(u>15&&u<24)||(u>27&&u<36)||(u>39&&u<48)||(u>51&&u<60)||(u>63&&u<72)||(u>75&&u<84)||(u>87&&u<96)){
+                    if(nappeja[u].getText().equals(kirjain)){
+                                if(nappeja[u+11].getText().equals(kirjain)){
+                                    if(nappeja[u+22].getText().equals(kirjain)){
+                                        if(nappeja[u+33].getText().equals(kirjain)){
+                                            if(nappeja[u+44].getText().equals(kirjain)){
+                                                voittaja = kirjain;
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                    }
+                if ((u<8)||(u<20&&u>11)||(u<32&&u>23)||(u<44&&u>35)||(u<56&&u>47)||(u<68&&u>59)||(u<80&&u>71)||(u<92&&u>83)||(u<104&&u>95)||(u<116&&u>107)||(u<128&&u>119)||(u<140&&u>131)){
+                    if(nappeja[u].getText().equals(kirjain)){
+                        if(nappeja[u+1].getText().equals(kirjain)){
+                            if(nappeja[u+2].getText().equals(kirjain)){
+                                if(nappeja[u+3].getText().equals(kirjain)){
+                                    if(nappeja[u+4].getText().equals(kirjain)){
+                                        voittaja = kirjain;
+                                }
+                            }
+                        }
+                    }
+                }
+                    if (u<92){
+                        if(nappeja[u].getText().equals(kirjain)){
+                            if(nappeja[u+13].getText().equals(kirjain)){
+                                if(nappeja[u+26].getText().equals(kirjain)){
+                                    if(nappeja[u+39].getText().equals(kirjain)){
+                                        if(nappeja[u+52].getText().equals(kirjain)){
+                                            voittaja = kirjain;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                
+            
         
+                u++;
+                }    
+                if (voittaja.equals("X") && i == 0){
+                    frame.setVisible(false);
+                    voittoIkkuna(this.yksi);
+                    System.out.println("Onneksi olkoon " + this.yksi.pelaajanNimi() + "! VOITIT PELIN!");
+                }
+                if (voittaja.equals("O") && i == 1){
+                    System.out.println("Onneksi olkoon " + this.kaksi.pelaajanNimi() + "! VOITIT PELIN!");
+                }
+                i++;
+            }
+        }
+        
+        public void voittoIkkuna(Pelaaja pelaaja){
+            JFrame voittoRuutu = new JFrame("Voittaja: " + pelaaja.pelaajanNimi());
+        voittoRuutu.setPreferredSize(new Dimension(400, 300));
+        
+        voittoRuutu.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        
+        Container container = voittoRuutu.getContentPane();
+        GridLayout layout = new GridLayout(2,1);
+        container.setLayout(layout);
+        
+        
+        
+        JPanel alaPaneeli = new JPanel(new GridLayout(1, 3));
+        
+        JButton joo = new JButton("Uusi peli");
+        JButton ei = new JButton("Sulje");
+        JButton scoret = new JButton("Tuloslista (tulossa)");
+        voittoIkkunanNapinKuuntelija kuuntelija = new voittoIkkunanNapinKuuntelija(joo, ei, scoret, voittoRuutu);
+        joo.addActionListener(kuuntelija);
+        ei.addActionListener(kuuntelija);
+        scoret.addActionListener(kuuntelija);
+        alaPaneeli.add(joo);
+        alaPaneeli.add(ei);
+        alaPaneeli.add(scoret);
+        
+        container.add(new JLabel("Onneksi olkoon " + pelaaja.pelaajanNimi() + "! Voitit pelin! Pelataanko uudestaan?"), BorderLayout.NORTH);
+        container.add(alaPaneeli, BorderLayout.SOUTH);
+        
+        voittoRuutu.pack();
+        voittoRuutu.setVisible(true);
+        }
         public String toString(){
             return "Pelaaja X: " + this.yksi + ", Pelaaja O: " + this.kaksi + ". Vaikeusaste: " + this.vaikeus;
         }
